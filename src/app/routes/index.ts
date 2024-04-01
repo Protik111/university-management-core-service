d61be550736dd7cbf16c79345168d1c7409ca043
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-04-02 00:24:15 +0600

    [NOT DONE] semester create

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { AcademicSemesterRoutes } from '../moduels/academicSemester/academicSemester.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/academic-semesters',
+    route: AcademicSemesterRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
