506b9ca6090f3e1de49d0f80ab984955858f3792
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-04-03 05:35:24 +0600

    [DONE] get all semester with pagination

--- src/app/moduels/academicSemester/academicSemester.constants.ts
+++ src/app/moduels/academicSemester/academicSemester.constants.ts
@@ -1,1 +1,13 @@
+export const AcademicSemesterSearchAbleFields = [
+  'title',
+  'code',
+  'startMonth',
+  'endMonth',
+];
 
+export const AcademicSemesterFilterAbleFileds = [
+  'searchTerm',
+  'code',
+  'startMonth',
+  'endMonth',
+];
--- src/app/moduels/academicSemester/academicSemester.controller.ts
+++ src/app/moduels/academicSemester/academicSemester.controller.ts
@@ -2,7 +2,9 @@ import { AcademicSemester } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterFilterAbleFileds } from './academicSemester.constants';
 import { AcademicSemesterService } from './academicSemester.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -16,6 +18,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, AcademicSemesterFilterAbleFileds);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic semester data fetched',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicSemesterController = {
   insertIntoDB,
+  getAllFromDB,
 };
