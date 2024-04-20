158825ceb9d506adaebf6fd24ed3396125c9c040
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-04-20 12:07:41 +0600

    [DONE] room module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import { academicDepartmentRoutes } from '../moduels/academicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../moduels/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../moduels/academicSemester/academicSemester.routes';
+import { buildingRoutes } from '../moduels/building/building.routes';
+import { courseRoutes } from '../moduels/course/course.routes';
+import { facultyRoutes } from '../moduels/faculty/faculty.routes';
+import { roomRoutes } from '../moduels/room/room.routes';
+import { studentRoutes } from '../moduels/student/student.routes';
 
 const router = express.Router();
 
@@ -8,6 +15,34 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: academicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: facultyRoutes,
+  },
+  {
+    path: '/students',
+    route: studentRoutes,
+  },
+  {
+    path: '/buildings',
+    route: buildingRoutes,
+  },
+  {
+    path: '/rooms',
+    route: roomRoutes,
+  },
+  {
+    path: '/courses',
+    route: courseRoutes,
   },
 ];
 
