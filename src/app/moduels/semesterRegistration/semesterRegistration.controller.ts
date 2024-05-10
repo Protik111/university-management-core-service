3c5355c3c7e3dcd5a54cead7d3de8d01dfa411fe
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-05-10 20:38:45 +0600

    [DONE] semester registration

--- src/app/moduels/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/moduels/semesterRegistration/semesterRegistration.controller.ts
@@ -43,6 +43,17 @@ const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await SemesterRegistrationService.updateOneInDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'SemesterRegistration updated successfully',
+    data: result,
+  });
+});
+
 const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await SemesterRegistrationService.deleteByIdFromDB(id);
@@ -58,5 +69,6 @@ export const SemesterRegistrationController = {
   insertIntoDB,
   getAllFromDB,
   getByIdFromDB,
+  updateOneInDB,
   deleteByIdFromDB,
 };
