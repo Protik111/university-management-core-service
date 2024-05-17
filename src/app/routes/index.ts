6d0f6895264444b7fa71878c73dc0fe2afccb2cd
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-05-17 22:38:49 +0600

    [DONE] all routes added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { academicDepartmentRoutes } from '../moduels/academicDepartment/academicDepartment.routes';
 import { academicFacultyRoutes } from '../moduels/academicFaculty/academicFaculty.routes';
-import { AcademicSemesterRoutes } from '../moduels/academicSemester/academicSemester.routes';
+import { AcademicSemeterRoutes } from '../moduels/academicSemester/academicSemester.routes';
 import { buildingRoutes } from '../moduels/building/building.routes';
 import { courseRoutes } from '../moduels/course/course.routes';
 import { facultyRoutes } from '../moduels/faculty/faculty.routes';
+import { offeredCourseRoutes } from '../moduels/offeredCourse/offeredCourse.routes';
+import { offeredCourseClassScheduleRoutes } from '../moduels/offeredCourseClassSchedule/offeredCourseClass.routes';
+import { offeredCourseSectionRoutes } from '../moduels/offeredCourseSection/offeredCourseSection.routes';
 import { roomRoutes } from '../moduels/room/room.routes';
+import { semesterRegistrationRoutes } from '../moduels/semesterRegistration/semesterRegistration.routes';
 import { studentRoutes } from '../moduels/student/student.routes';
+import { studentEnrolledCourseMarkRoutes } from '../moduels/studentEnrolledCourseMark/studentEnrolledCourseMark.route';
 
 const router = express.Router();
 
@@ -14,7 +19,7 @@ const moduleRoutes = [
   // ... routes
   {
     path: '/academic-semesters',
-    route: AcademicSemesterRoutes,
+    route: AcademicSemeterRoutes,
   },
   {
     path: '/academic-faculties',
@@ -43,6 +48,26 @@ const moduleRoutes = [
   {
     path: '/courses',
     route: courseRoutes,
+  },
+  {
+    path: '/semester-registration',
+    route: semesterRegistrationRoutes,
+  },
+  {
+    path: '/offered-courses',
+    route: offeredCourseRoutes,
+  },
+  {
+    path: '/offered-course-sections',
+    route: offeredCourseSectionRoutes,
+  },
+  {
+    path: '/offered-course-class-schedules',
+    route: offeredCourseClassScheduleRoutes,
+  },
+  {
+    path: '/student-enrolled-course-marks',
+    route: studentEnrolledCourseMarkRoutes,
   },
 ];
 
